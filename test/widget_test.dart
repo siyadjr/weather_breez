1eb5ad9c5615c9569cbcf79f4675df0cc9714d6d
Siyad jr <siyadsiyad016@gmail.com>
2025-02-28 16:46:39 +0530

    Added read.me file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
-import 'package:weatherbreez/localization/local.dart';
+
 
 // Generate mocks with build_runner: flutter pub run build_runner build
 @GenerateMocks([FlutterLocalization])
